8c39e241aec023741ba1f1e2fbf490f992a059e6
kilb <github@bll.io>
2024-01-28 05:12:18 +0800

    optimize

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app("/var/mythapi/tsniffer/data")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, workers=8, dev=True)
