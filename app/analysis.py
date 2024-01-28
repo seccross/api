14e4fbe693d6af1917da2115bbfaf0c29ef84674
kilb <github@bll.io>
2024-01-28 22:32:05 +0800

    add code

--- app/analysis.py
+++ app/analysis.py
@@ -2,8 +2,11 @@ import asyncio
 import subprocess
 import os
 
-myth = '/usr/local/bin/myth'
-params = ['analyze', '--parallel-solving', '-o', 'jsonv2']
+ts_myth = '/usr/local/bin/myth'
+ts_params = ['analyze', '--parallel-solving', '-o', 'jsonv2']
+
+xg_slither = '/usr/local/xguard/bin/slither'
+xg_params = ['--checklist', '--show-ignored-findings']
 
 async def run_myth(file, contract_name, args, session_dir):
 
@@ -20,11 +23,11 @@ async def run_myth(file, contract_name, args, session_dir):
     with open(saved_file_path, 'wb') as saved_file:
         saved_file.write(file.body)
     
-    args = params + [f"{saved_file_path}:{contract_name}"] + args
+    args = ts_params + [f"{saved_file_path}:{contract_name}"] + args
 
     # 异步运行myth程序
     process = await asyncio.create_subprocess_exec(
-        myth, *args,
+        ts_myth, *args,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
@@ -35,3 +38,40 @@ async def run_myth(file, contract_name, args, session_dir):
     if process.returncode == 0:
         with open(err_file_path, 'wb') as err_file:
             err_file.write(stderr)
+
+# 1: 异常退出；2：发现bug；0：未发现bug
+async def run_slither(file, solc_version, args, session_dir):
+
+    os.makedirs(session_dir, exist_ok=True)
+
+    original_file_name = file.name
+
+    result_file_path = os.path.join(session_dir, f"result.json")
+    report_file_path = os.path.join(session_dir, f"report.md")
+    err_file_path = os.path.join(session_dir, f"error.json")
+    ok_file_path = os.path.join(session_dir, f"OK")
+    # 将上传的文件也保存在相同的目录下
+    saved_file_path = os.path.join(session_dir, original_file_name)
+
+    # 保存上传的文件
+    with open(saved_file_path, 'wb') as saved_file:
+        saved_file.write(file.body)
+    
+    args =  [f"{saved_file_path}", ] + xg_params + ["--solc-solcs-select", f"{solc_version}", "--json", f"{result_file_path}"] + args
+
+    # 异步运行myth程序
+    process = await asyncio.create_subprocess_exec(
+        xg_slither, *args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+
+    with open(report_file_path, 'wb') as report_file:  
+        report_file.write(stdout)
+    if process.returncode == 1:
+        with open(err_file_path, 'wb') as err_file:
+            err_file.write(stderr)
+    if process.returncode == 0:
+        with open(ok_file_path, 'w') as ok_file:
+            ok_file.write('OK')
--- app/api.py
+++ app/api.py
@@ -59,5 +59,60 @@ def create_app(storage_dir):
         if os.path.exists(session_dir):
             return response.json({"status": "Processing"}, status=202)
         return response.json({"error": "Request not found"}, status=404)
+    
+    @app.post("/xg/analyze")
+    async def xg_analyze_file(request):
+        file = request.files.get('file')
+        solc_version = request.form.get('solc_version')  # 假设参数以JSON格式传递
+        args = request.form.get('args')  # 假设参数以JSON格式传递
+
+        if not file:
+            return response.json({"error": "No file provided"}, status=400)
+
+        # 解析参数
+        try:
+            args = json.loads(args) if args else []
+        except json.JSONDecodeError:
+            return response.json({"error": "Invalid JSON format in args"}, status=400)
+        
+         # 确保存储目录存在
+        os.makedirs(storage_dir, exist_ok=True)
+
+        request_id = str(uuid.uuid4())
+        session_dir = dir_path(request_id)
+
+         # 异步执行myth分析
+        asyncio.create_task(run_myth(file, solc_version, args, session_dir))
+    
+        # 返回request id
+        return response.json({"request_id": request_id})
+    
+    @app.get("/xg/result/<request_id>")
+    async def get_result(request, request_id):
+        # 构建结果文件的路径
+        session_dir = dir_path(request_id)
+        result_file_path = os.path.join(session_dir, f"result.json")
+        report_file_path = os.path.join(session_dir, f"report.md")
+        error_file_path = os.path.join(session_dir, f"error.json")
+        ok_file_path = os.path.join(session_dir, f"OK")
+
+        if os.path.exists(error_file_path):
+            with open(result_file_path, 'r') as file:
+                error = file.read()
+            return response.json({"error": error}, status=422)
+        
+        pass_check = True if os.path.exists(ok_file_path) else False
+        
+        if os.path.exists(result_file_path):
+            with open(result_file_path, 'r') as file:
+                result = json.load(file)
+                with open(report_file_path, 'r') as file:
+                    report = file.read()
+                return response.json({"passed": pass_check, "result": result, "report": report})
+        
+        if os.path.exists(session_dir):
+            return response.json({"status": "Processing"}, status=202)
+        
+        return response.json({"error": "Request not found"}, status=404)
 
     return app
