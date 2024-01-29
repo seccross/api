e9d44e441b4cf800aa20123a53245a4ef1f88754
kilb <github@bll.io>
2024-01-29 11:15:50 +0800

    fix bug

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app("/var/mythapi/data")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, workers=8, dev=True)
+    app.run(host="127.0.0.1", port=8000, workers=4, dev=True)
